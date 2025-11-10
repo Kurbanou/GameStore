a7c595c9e52526ced16d3ef5815d40b2d6f82102
Фарид Курбанов <kf.dev@german-web.ru>
2025-11-10 20:22:51 +0300

    core-gamestore plugin add

---
+++ plugins/core-gamestore/gamestore-general.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Core Gamestore
+ * Description: Core  Code for Gamestore
+ * Version: 1.0
+ * Author: Farid
+ * Text Domain: core-gamestore
+ * Domain Path: /languages
+ */
+
+define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('GAMESTORE_PLUGIN_DIR', plugin_dir_path(__FILE__));
